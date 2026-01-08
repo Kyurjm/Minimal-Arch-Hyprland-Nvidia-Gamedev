083f79eb0afeb3b1021ee90dd8a41f8aa55866e6
kyurjm <kyurjm@gmail.com>
2026-01-09 06:46:53 +0700

    Cleanup

--- CONFIG/nvim/init.lua
+++ CONFIG/nvim/init.lua
@@ -1,4 +1,7 @@
--- OPTIONS
+--###################
+--##### OPTIONS #####
+--###################
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.o.number = true
@@ -19,7 +22,7 @@ vim.o.smartcase = true
 vim.o.hlsearch = true
 vim.o.incsearch = true
 vim.o.signcolumn = 'yes'
-vim.o.updatetime = 250
+vim.o.updatetime = 555
 vim.o.timeoutlen = 3000
 vim.o.splitright = true
 vim.o.splitbelow = true
@@ -39,12 +42,14 @@ vim.api.nvim_set_hl(0, 'YankHighlight', { bg = '#c1a473', fg = '#1F1F28' }) vim.
 vim.opt.guicursor = { "n-v-c:block", "i-ci-ve:ver25", "r-cr:hor20", "o:hor50", "a:blinkon150-blinkoff150-blinkwait100"}
 vim.diagnostic.config({virtual_text = true,signs = true,underline = true,update_in_insert = false,})
 
--- PLUGINS
+--###################
+--##### PLUGINS #####
+--###################
+
 vim.pack.add({
   { src = "https://github.com/rebelot/kanagawa.nvim" },
   { src = "https://github.com/nvimdev/indentmini.nvim" },
   { src = "https://github.com/sphamba/smear-cursor.nvim" },
-  { src = "https://github.com/mbbill/undotree" },
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
@@ -57,9 +62,7 @@ vim.pack.add({
   { src = "https://github.com/folke/which-key.nvim" },
   { src = "https://github.com/nvim-telescope/telescope.nvim" },
   { src = "https://github.com/nvim-telescope/telescope-fzf-native.nvim" },
-  { src = "https://github.com/nvim-neo-tree/neo-tree.nvim" },
   { src = "https://github.com/nvim-lua/plenary.nvim" },
-  { src = "https://github.com/MunifTanjim/nui.nvim" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
   { src = "https://github.com/nvim-lualine/lualine.nvim" },
   { src = "https://github.com/kdheepak/lazygit.nvim" },
@@ -70,40 +73,44 @@ vim.pack.add({
   { src = "https://github.com/karb94/neoscroll.nvim" },
 })
 
--- CONFIG
-require('neoscroll').setup() -- Neoscroll
+--###################
+--##### CONFIGS #####
+--###################
+
+-- Neoscroll
+require('neoscroll').setup()
 local neoscroll = require('neoscroll')
-require('telescope').load_extension('lazygit') -- LazyGit
-require('lualine').setup({}) -- lualine
-require("nvim-autopairs").setup({}) -- Autopairs
-vim.cmd("colorscheme kanagawa-wave") -- Kanagawa
-require("smear_cursor").setup({ time_interval = 7 }) -- Smear Cursor
-require("indentmini").setup({ char = "┊", }) -- Indentmini
+
+-- Lazygit
+require('telescope').load_extension('lazygit')
+
+-- Lualine
+require('lualine').setup({})
+
+-- Autopairs
+require("nvim-autopairs").setup({})
+
+-- Kanagawa
+vim.cmd("colorscheme kanagawa-wave")
+
+-- Smear Cursor
+require("smear_cursor").setup({ time_interval = 7 })
+
+-- Indentmini
+require("indentmini").setup({ char = "┊", })
 vim.cmd.highlight('IndentLine guifg=#363646')
 vim.cmd.highlight('IndentLineCurrent guifg=#7E9CD8')
-local wk = require("which-key") -- Whichkey keymaps list
+
+-- Whichkey Keymap Guide
+local wk = require("which-key")
 wk.setup({preset = "helix",})
-vim.g.loaded_netrwPlugin = 1 -- Yazi
+
+-- Yazi
+vim.g.loaded_netrwPlugin = 1
 vim.api.nvim_create_autocmd("UIEnter", {callback = function()require("yazi").setup({open_for_directories = true,})end,})
-require("neo-tree").setup({ -- Neotree
-  clipboard = {
-    sync = "universal",
-  },
-  default_component_configs = {
-    indent = {
-      indent_marker = "┊",
-    },
-  },
-  filesystem = {
-    filtered_items = {
-      hide_dotfiles = false,
-      hide_gitignored = false,
-      hide_ignored = false,
-    },
-    hide_hidden = true,
-  },
-})
-local ok, telescope = pcall(require, "telescope") -- Telescope
+
+-- Telescope
+local ok, telescope = pcall(require, "telescope")
 if not ok then
   return
 end
@@ -141,7 +148,9 @@ vim.api.nvim_create_autocmd("PackChanged", {
     end
   end,
 })
-require("mason").setup() -- LSP, Treesitter & Auto Completion Support
+
+-- LSP
+require("mason").setup()
 require("mason-lspconfig").setup({
   ensure_installed = {
     "lua_ls", -- Lua
@@ -178,7 +187,9 @@ require("mason-tool-installer").setup({
     "shellcheck", -- Bash linter
   },
 })
-local ts_ok, ts_configs = pcall(require, 'nvim-treesitter.configs') -- Treesitter
+
+-- Treesitter
+local ts_ok, ts_configs = pcall(require, 'nvim-treesitter.configs')
 if ts_ok then
   ts_configs.setup({
     ensure_installed = {
@@ -192,12 +203,16 @@ if ts_ok then
     indent = { enable = true },
   })
 end
-require("luasnip.loaders.from_vscode").lazy_load() -- Luasnip
+
+-- Luasnip
+require("luasnip.loaders.from_vscode").lazy_load()
 local ls_ok, _ = pcall(require, "luasnip")
 if ls_ok then
   require("luasnip.loaders.from_vscode").lazy_load()
 end
-require('blink.cmp').setup({ -- Blink
+
+-- Blink Auto Completion
+require('blink.cmp').setup({
   snippets = { preset = 'luasnip' },
   sources = {
     default = { 'lsp', 'path', 'snippets', 'buffer' },
@@ -239,7 +254,9 @@ vim.api.nvim_create_autocmd("PackChanged", {
     end
   end,
 })
-require("dapui").setup() -- DAP
+
+-- DAP
+require("dapui").setup()
 local dap = require("dap")
 local dapui = require("dapui")
 dapui.setup()
@@ -252,22 +269,9 @@ end
 dap.listeners.before.event_exited["dapui"] = function()
   dapui.close()
 end
-dap.adapters.coreclr = { -- Unity Debugger
-  type = "executable",
-  command = vim.fn.stdpath("data") .. "/mason/bin/netcoredbg",
-  args = { "--interpreter=vscode" },
-}
-dap.configurations.cs = {
-  {
-    type = "coreclr",
-    name = "Launch Unity / .NET",
-    request = "launch",
-    program = function()
-      return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-    end,
-  },
-}
-dap.adapters.python = { -- Python Debugger
+
+-- Python DAP
+dap.adapters.python = {
   type = "executable",
   command = vim.fn.stdpath("data") .. "/mason/bin/python3",
   args = { "-m", "debugpy.adapter" },
@@ -281,6 +285,8 @@ dap.configurations.python = {
     pythonPath = "python",
   },
 }
+
+-- Rust DAP
 dap.adapters.codelldb = {
   type = "server",
   port = "${port}",
@@ -289,7 +295,7 @@ dap.adapters.codelldb = {
     args = { "--port", "${port}" },
   },
 }
-dap.configurations.rust = { -- Rust Debugger
+dap.configurations.rust = {
   {
     name = "Launch",
     type = "codelldb",
@@ -302,33 +308,47 @@ dap.configurations.rust = { -- Rust Debugger
   },
 }
 
--- KEYMAPS
-vim.keymap.set("n", "<leader>U", function() if vim.pack and vim.pack.update then vim.pack.update() else vim.notify("vim.pack.update() not available in this Neovim build", vim.log.levels.WARN) end end, { desc = "Update plugins (vim.pack)" })
+--###################
+--##### KEYMAPS #####
+--###################
+
+-- Update Packages
+vim.keymap.set("n", "<leader>u", function() if vim.pack and vim.pack.update then vim.pack.update() else vim.notify("vim.pack.update() not available in this Neovim build", vim.log.levels.WARN) end end, { desc = "Update plugins (vim.pack)" })
+
+-- Clear highlight search
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>t', '<cmd>botright split | terminal<CR><cmd>resize 10<CR>i', { desc = 'Open Terminal' })
+
+-- Terminal
+vim.keymap.set('n', '<leader>th', '<cmd>botright split | terminal<CR><cmd>resize 15<CR>i', { desc = 'Bottom' })
+vim.keymap.set('n', '<leader>tv', '<cmd>vsp | terminal<CR><cmd>vertical resize 40<CR>i', { desc = 'Right' })
+require("which-key").add({ { "<leader>t", group = "Terminal" } })
+
+-- Move cursor between windows
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, { desc = 'Toggle UndoTree' })
-vim.keymap.set('n', '<leader>n', '<Cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
-vim.api.nvim_set_keymap('n', '<leader>of', ':Telescope find_files<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>og', ':Telescope live_grep<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>l', "<cmd>LazyGit<cr>", {desc = "Open LazyGit"})
-vim.keymap.set("n", "<leader>e", function() require("yazi").yazi() end, { desc = 'Open Yazi' })
+
+-- Telescope
+vim.api.nvim_set_keymap('n', '<leader>s', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+
+-- Lazygit
+vim.keymap.set('n', '<leader>l', "<cmd>LazyGit<cr>", {desc = "LazyGit"})
+
+-- Yazi
+vim.keymap.set("n", "<leader>e", function() require("yazi").yazi() end, { desc = 'Yazi' })
+
+-- Refractor
 vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { desc = 'Rename Variable' })
+
+-- Formatter
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format file" })
-vim.keymap.set("v", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format selection" })
-vim.keymap.set('n', '<leader>dd', vim.diagnostic.setloclist, { desc = 'Open diagnostic list' })
-vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP Continue" })
-vim.keymap.set("n", "<leader>dn", dap.step_over, { desc = "DAP Step Over" })
-vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "DAP Step Into" })
-vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "DAP Step Out" })
-vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
-vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "DAP REPL" })
-vim.keymap.set("n", "<leader>dl", dap.run_last, { desc = "DAP Run Last" })
-vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "DAP Terminate" })
-vim.keymap.set("n", "<leader>dB", function() dap.set_breakpoint(vim.fn.input("Breakpoint condition: ")) end, { desc = "DAP Conditional Breakpoint" })
-vim.keymap.set("n", "<leader>du", function() dapui.toggle() end, { desc = "DAP UI Toggle" })
+
+-- Debugger
+vim.keymap.set("n", "<leader>d", function() dapui.toggle() end, { desc = "DAP UI Toggle" })
+
+-- Neoscroll
 vim.keymap.set("n", '<S-Up>', function() neoscroll.ctrl_b({ duration = 250 }) end)
 vim.keymap.set("n", '<S-Down>', function() neoscroll.ctrl_f({ duration = 250 }) end)
+vim.keymap.set("n", '<C-Up>', function() neoscroll.ctrl_u({ duration = 250 }) end)
+vim.keymap.set("n", '<C-Down>', function() neoscroll.ctrl_d({ duration = 250 }) end)
